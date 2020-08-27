d14edcbccdb652a90627d50226aaae800a3d8d48
mark vader <m@tokio.ark>
2020-08-27 13:09:49 -0500

    mark vader

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
+// import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -73,7 +73,7 @@ const Navbar = class extends React.Component {
                 Contact
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Form Examples Mark Vader
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
